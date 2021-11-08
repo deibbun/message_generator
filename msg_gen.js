6a350aa29d997cd98dfe859d81c4b139b4bc83aa
doug houser <jdbinkley6@gmail.com>
2021-11-08 04:45:15 -0600

    Add first js file

---
+++ msg_gen.js
@@ -0,0 +1,30 @@
+const begin = [
+    'Create',
+    'Jump',
+    'Let\'s',
+    'Remove',
+    'Pull',
+    'Tug',
+    'Jack'
+];
+
+const middle = [
+    'something',
+    'off',
+    'go',
+    'your',
+    'it',
+    'my',
+    'me'
+];
+
+const last = [
+    'new',
+    'bridge',
+    'brandon',
+    'panties',
+    'out',
+    'cock',
+    'off'
+];
+
